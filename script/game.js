5522d2feccd1485cbf738b8b20130633c4353b70
Troy Kan <troy_kan@hotmail.com>
2022-12-01 04:05:20 -0800

    Update game.js

--- script/game.js
+++ script/game.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded",function(){
     generateDiv();
     startGame();
 })
-
+//generate divs for the board
 function generateDiv(){
     let row=20;
     let col=20;
@@ -25,41 +25,41 @@ function startGame(){
     var player = location[0];
     var ball = location[1];
     var defend = location[2];
-  
+    //event listener for user pressing the key
     document.addEventListener('keydown', function(event) {          
         // Set a direction for each code
         switch (event.code) {
             case 'KeyA':            //left
                 if(player%20 != 0)
                     player--;
-                if(player == ball)
-                    ball--;
+                if(player == ball)  
+                    ball--;         //kick ball left
                 defend++;
                 break;
             case 'KeyS':
                 if(player < 380 )   //down
                     player += 20;
                 if(player == ball)
-                    ball += 20;
+                    ball += 20;     //kick ball down
                 defend++;
                 break;
             case 'KeyD':            //right
                 if(player%20 != 19 )
                     player++;
                 if(player == ball)
-                    ball ++;
+                    ball ++;        //kick ball right
                 defend++;
                 break;
             case 'KeyW':            //up
                 if(player > 19 )
                     player -= 20;
                 if(player == ball)
-                    ball -= 20;
+                    ball -= 20;     //kick ball UP
                 defend++;
                 break;
         }
-        if(defend > 371)
-            defend = 368;
+        if(defend > 371)            //defender at the right end of the goal
+            defend = 368;           //shif the defender to the left
         location[0]=player;
         location[1]=ball;
         location[2]=defend;
@@ -68,27 +68,26 @@ function startGame(){
         gameEnd = check(location);
         if(gameEnd){
             clearScreen();
-            location[1]=21;
             document.removeEventListener('keydown', function(event) {
             });
         }
     });
 }
-
+//clear screen to display ground, boundary, and goal only
 function clearScreen(){    
     var divs = document.querySelectorAll("div");
     for(let i=0; i<divs.length; i++){
-        if(i>379 || i<20 || i%20 == 0 || i%20 == 19){
-            if(i>=388 && i<=391)
+        if(i>379 || i<20 || i%20 == 0 || i%20 == 19){   //boundary
+            if(i>=388 && i<=391)                        //goal
                 divs[i].className = 'bdiv';
             else
                 divs[i].className = 'wdiv';
         }
-        else
+        else                                             //ground
             divs[i].className = 'gdiv';
     }
 }
-
+//initilize postion for player, ball, and defender
 function initilize(){   
     var divs = document.querySelectorAll("div");
     var location = [];
@@ -103,26 +102,26 @@ function initilize(){
     location.push(defend);
     return location;
 }
-
+//display updated postion of player, ball, and defender
 function draw(location){
     var divs = document.querySelectorAll("div");
     divs[location[0]].className = 'rdiv';
     divs[location[1]].className = 'balldiv';
     divs[location[2]].className = 'bdiv';
 }
-
+//check if the game is end
 function check(location){
     var ball = location[1];
     var defend = location[2];
 
-    if ((ball>379 || ball<20 || ball%20 == 0 || ball%20 == 19)){
-        if(ball>=388 && ball<=391)
+    if ((ball>379 || ball<20 || ball%20 == 0 || ball%20 == 19)){            //on the boundary
+        if(ball>=388 && ball<=391)                                          //at the goal
             window.alert("Goal!!! You win. referesh to start a new game");
         else
             window.alert("Out of bounds... referesh to start a new game");
         return true;
     }
-    else if(ball == defend){
+    else if(ball == defend){                                                   //defender has the ball
         window.alert("Defender has the ball. You lose. referesh to start a new game");
         return true;
     }
